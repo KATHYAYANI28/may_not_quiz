d3bf2b0fc61114257ca7be88f6f0dbda77892c02
KATHYAYANI28 <lit2021044@iiitl.ac.in>
2023-02-01 23:55:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/maynotbe/loginIntro.kt
@@ -0,0 +1,11 @@
+package com.example.maynotbe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class loginIntro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_intro)
+    }
+}
---
+++ app/src/main/java/com/example/maynotbe/signup.kt
@@ -0,0 +1,19 @@
+package com.example.maynotbe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+
+class signup : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+
+    }
+    private fun signUpUser(){
+
+
+    }
+}
